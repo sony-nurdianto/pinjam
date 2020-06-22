ecb165fd68e73796314142695511cd182e420b8a
sony-nurdianto <sonynurdianto445@gmail.com>
2020-06-22 15:00:28 +0700

    try to find bug deploy

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      {/* <Navbar /> */}
-      <Switch>
-        {/* <Route exact path="/"><Home /></Route> */}
-      </Switch>
-    </Router>
+    <div>halo</div>
+    // <Router>
+    //    <Navbar /> 
+    //   <Switch>
+    //     <Route exact path="/"><Home /></Route>
+    //   </Switch>
+    // </Router>
   );
 }
 
