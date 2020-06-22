4974ff8527723f001029a56bcf991171ad2cdb17
sony-nurdianto <sonynurdianto445@gmail.com>
2020-06-22 15:04:53 +0700

    zero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Navbar from './components/Navbar.js'
 // import Home from './Pages/Home'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // import "./style.css"
 
 // https://www.youtube.com/watch?v=FZQxPTV3cFk
