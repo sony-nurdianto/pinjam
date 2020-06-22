e0d47b7c1a1a6a40e41805e2fc421b9c4c9465a3
sony-nurdianto <sonynurdianto445@gmail.com>
2020-06-22 13:59:04 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar.js'
+// import Home from './Pages/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import "./style.css"
+
+// https://www.youtube.com/watch?v=FZQxPTV3cFk
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path="/"><Home /></Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { Navbar, Nav, NavItem } from "react-bootstrap"
+import "./Navbar.css"
+// import "../../style.css"
+
+
+const Navbars = () => {
+
+    return (
+
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+            <Navbar.Brand href="#home"><strong className="brand"><span className="brand1">B<span className="brand2">e</span>li Baik</span></strong></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link to="/"><strong>Tentang</strong></Nav.Link>
+                    <Nav.Link to="/"><strong>Fitur & Bank Pilihan</strong></Nav.Link>
+                    <Nav.Link to="/"><strong>Testimoni</strong></Nav.Link>
+                    <Nav.Link to="/"><strong>Kontak</strong></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+
+}
+
+export default Navbars
